58e0cc7b114408feedc508d8508279f7b494bd1e
Nick Reid <nickreid@nickreid.com>
2011-12-24 11:24:59 -0800

    updated footer styles

--- functions.php
+++ functions.php
@@ -1,5 +1,24 @@
 <?php
 
+add_action( 'widgets_init', 'nr_register_sidebars' );
+
+function nr_register_sidebars(){
+	register_sidebar(array(
+	  'name' => 'Footer_Right_Sidebar',
+	  'id' => 'footer-right-sidebar',
+	  'description' => 'Widgets in this area will be shown on the right-hand side of the footer.',
+	  'before_title' => '<h3>',
+	  'after_title' => '</h3>'
+	));
+	register_sidebar(array(
+	  'name' => 'Footer_Left_Sidebar',
+	  'id' => 'footer-left-sidebar',
+	  'description' => 'Widgets in this area will be shown on the left-hand side of the footer.',
+	  'before_title' => '<h3>',
+	  'after_title' => '</h3>'
+	));
+}
+
 function nr12_head(){
 	echo '<link rel="stylesheet" href="/wp-content/themes/nickreid2012/css/full_styles.css" />';
 }
@@ -14,10 +33,14 @@ add_action('roots_header_inside',nr12_header);
 
 
 function nr12_footer(){
-	/*
 	echo '<div class="container">';
+	echo '<ul class="sidebar left">';
+	dynamic_sidebar( 'Footer_Left_Sidebar' );
+	echo '</ul>';
+	echo '<ul class="sidebar right">';
+	dynamic_sidebar( 'Footer_Right_Sidebar' );
+	echo '</ul>';
 	echo '</div>';
-	*/
 }
 add_action('roots_footer_inside',nr12_footer);
 
